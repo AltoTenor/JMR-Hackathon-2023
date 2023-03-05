e338acd7512484e03c2f1f2d934d5e9202c7a340
AltoTenor <theonetrueking2002@gmail.com>
2023-03-05 11:57:31 +0530

    fixed

--- det.py
+++ det.py
@@ -1,5 +1,5 @@
 from roboflow import Roboflow
-rf = Roboflow(api_key="Qbc4lG4O8U7k5TD1AKCh")
+rf = Roboflow(api_key="API KEY")
 project = rf.workspace().project("crickettrack")
 model = project.version(1).model
 
